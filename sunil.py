87f07689dfdc42cedb6bddcb6932df580319cd72
sunilkumarbalaga <38848759+sunilkumarbalaga@users.noreply.github.com>
2018-05-28 16:38:46 +0530

    Update sunil.py

--- sunil.py
+++ sunil.py
@@ -2,7 +2,7 @@
 
 import psycopg2
 
-virat_1 = 'What are the most popular three articles of all time?'
+virat_1 = 'The most popular three article are'
 sunil_1 = """
 select title, count(*) as views from articles inner join
 log on concat('/article/', articles.slug) = log.path
@@ -10,7 +10,7 @@ where log.status like '%200%'
 group by log.path, articles.title order by views desc limit 3;
 """
 
-virat_2 = 'Who are the most popular article authors of all time?'
+virat_2 = 'The most popular article authors'
 sunil_2 = """
 select authors.name, count(*) as views from articles inner join
 authors on articles.author = authors.id inner join
@@ -18,7 +18,7 @@ log on concat('/article/', articles.slug) = log.path where
 log.status like '%200%' group by authors.name order by views desc
 """
 
-virat_3 = 'On which days did more than 1% of requests lead to errors?'
+virat_3 = 'Days with more thna 1% errors'
 sunil_3 = """
 select * from (
     select a.day,
