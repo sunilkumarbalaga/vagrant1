4862459d0e6fb8c7209430a593341bbfea86d306
sunilkumarbalaga <38848759+sunilkumarbalaga@users.noreply.github.com>
2018-05-28 16:55:03 +0530

    Update sunil.py

--- sunil.py
+++ sunil.py
@@ -2,24 +2,24 @@
 
 import psycopg2
 
-virat_1 = 'The most popular three article are'
-sunil_1 = """
+question_1 = 'The most popular three articles:-'
+invest_1 = """
 select title, count(*) as views from articles inner join
 log on concat('/article/', articles.slug) = log.path
 where log.status like '%200%'
 group by log.path, articles.title order by views desc limit 3;
 """
 
-virat_2 = 'The most popular article authors'
-sunil_2 = """
+question_2 = 'The most popular article authors:-'
+invest_2 = """
 select authors.name, count(*) as views from articles inner join
 authors on articles.author = authors.id inner join
 log on concat('/article/', articles.slug) = log.path where
 log.status like '%200%' group by authors.name order by views desc
 """
 
-virat_3 = 'Days with more thna 1% errors'
-sunil_3 = """
+question_3 = 'Days with more than 1% errors:-'
+invest_3 = """
 select * from (
     select a.day,
     round(cast((100*b.hits) as numeric) / cast(a.hits as numeric), 2)
@@ -39,16 +39,16 @@ class investigation:
             self.db = psycopg2.connect('dbname=news')
             self.cursor = self.db.cursor()
         except Exception as o:
-            print o
+            print (o)
 
-    def execute_sunil(self, sunil):
-        self.cursor.execute(sunil)
+    def execute_invest(self, invest):
+        self.cursor.execute(invest)
         return self.cursor.fetchall()
 
-    def solve(self, virat, sunil, suffix='views'):
-        sunil = sunil.replace('\n', ' ')
-        result = self.execute_sunil(sunil)
-        print virat
+    def solve(self, question, invest, suffix='views'):
+        invest = invest.replace('\n', ' ')
+        result = self.execute_invest(invest)
+        print question
         for v in range(len(result)):
             print '\t', v + 1, '.', result[v][0], '--', result[v][1], suffix
         # blank line
@@ -60,7 +60,7 @@ class investigation:
 
 if __name__ == '__main__':
     investigation = investigation()
-    investigation.solve(virat_1, sunil_1)
-    investigation.solve(virat_2, sunil_2)
-    investigation.solve(virat_3, sunil_3, '% error')
+    investigation.solve(question_1, invest_1)
+    investigation.solve(question_2,invest_2)
+    investigation.solve(question_3, invest_3, '% error')
 investigation.exit()
